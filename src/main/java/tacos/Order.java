19f7028960427ec161d648e1e5d2c009fb780e7a
Daniel J <daniel.jeczen@edu.uekat.pl>
2019-03-25 20:40:54 +0100

    Initial commit

---
+++ src/main/java/tacos/Order.java
@@ -0,0 +1,36 @@
+package tacos;
+
+
+import java.util.Date; //java.util.date vs java.sql.date
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.CreditCardNumber;
+// When it comes to list you cannot use notBlank, it's better to write your own annotation
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class Order {
+
+    private Long id;
+    private Date placedAt;
+
+    @NotBlank(message="Name is required")
+    private String name;
+    @NotBlank(message="Street is required")
+    private String street;
+    @NotBlank(message="City is required")
+    private String city;
+    @NotBlank(message="State is required")
+    private String state;
+    @NotBlank(message="Zip code is required")
+    private String zip;
+    @CreditCardNumber(message="Not a valid credit card number")
+    private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+             message="Must be formated MM/YY")
+    private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    private String ccCVV;
+
+}
---
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,79 @@
+package tacos.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Order;
+import tacos.Taco;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("order")
+public class DesignTacoController
+{
+    private final IngredientRepository ingredientRepo;
+
+    private TacoRepository designRepo;
+
+    @Autowired
+    public DesignTacoController(
+            IngredientRepository ingredientRepo,
+            TacoRepository designRepo) {
+        this.ingredientRepo = ingredientRepo;
+        this.designRepo = designRepo;
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    ingredients.stream().filter(line -> type.equals(line.getType())).collect(Collectors.toList()));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @PostMapping
+    public String processDesign(
+            @Valid Taco design, Errors errors,
+            @ModelAttribute Order order){
+
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
+
+        return "redirect:/orders/current";
+    }
+}
